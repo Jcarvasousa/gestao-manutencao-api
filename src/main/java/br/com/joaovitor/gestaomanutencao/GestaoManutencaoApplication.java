95b4c9373322e67e9475f836c46180356e32e6fc
Joao Vitor C. Sousa <jvc.carvalho14@gmail.com>
2026-09-17 18:53:32 -0300

    chore: setup inicial do projeto Spring Boot + Postgres

---
+++ src/main/java/br/com/joaovitor/gestaomanutencao/GestaoManutencaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaovitor.gestaomanutencao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoManutencaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoManutencaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/joaovitor/gestaomanutencao/GestaoManutencaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaovitor.gestaomanutencao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoManutencaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
